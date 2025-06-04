130c8c2364b1e037577efc62f8b5d475bde7ac45
Mohammad Lukman Aqib <mohammadlukmanaqib@gmail.com>
2025-06-04 14:27:24 +0700

    setup dan hello-world

---
+++ src/hello-world/HelloWorld.jsx
@@ -0,0 +1,8 @@
+export default function HelloWorld() {
+    return (
+        <div>
+            <h1>Hello, World!</h1>
+            <p>Mari Belajar React Demi Masa Depan yang Mbuh</p>
+        </div>
+    )
+}
