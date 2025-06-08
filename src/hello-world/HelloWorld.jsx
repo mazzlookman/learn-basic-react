491b0c306491506425172c61982233ee6a368cf2
Mohammad Lukman Aqib <mohammadlukmanaqib@gmail.com>
2025-06-08 17:49:10 +0700

    props

--- src/hello-world/HelloWorld.jsx
+++ src/hello-world/HelloWorld.jsx
@@ -1,8 +1,27 @@
 export default function HelloWorld() {
     return (
         <div>
-            <h1>Hello, World!</h1>
-            <p>Mari Belajar React Demi Masa Depan yang Mbuh</p>
+            <HeaderHelloWorld text="Hello, World! Props"/>
+            <BodyHelloWorld />
         </div>
+    )
+}
+
+function HeaderHelloWorld(props) {
+    const { text = 'Ups, lupa kasih text' } = props;
+    return (
+        <h1 style={{color: 'salmon'}}>{text.toUpperCase()}</h1>
+    )
+}
+
+function BodyHelloWorld() {
+    const text = 'Mari Belajar React Demi Masa Depan yang Mbuh';
+    const style = {
+        color: 'blue',
+        fontSize: '20px',
+        fontWeight: 'bold',
+    };
+    return (
+        <p style={style}>{text.toLowerCase()}</p>
     )
 }
